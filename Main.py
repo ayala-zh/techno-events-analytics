676e7015dc68f743b20dd51e1a603ba084eaaee9
ayala-zh <ayala.1805@mail.ru>
2025-10-04 13:46:32 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,33 @@
 import psycopg2
+import pandas as pd
+from export_excel import export_to_excel
 
 DB_NAME = "techno_events_db"
 DB_USER = "postgres"
 DB_PASSWORD = "0000"
 DB_HOST = "localhost"
 DB_PORT = "5432"
+
+def fetch_table_as_df(table_name):
+    """Забирает данные из таблицы и возвращает DataFrame"""
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT
+        )
+        query = f"SELECT * FROM {table_name};"
+        df = pd.read_sql(query, connection)
+        return df
+    except Exception as e:
+        print(f"Error fetching {table_name}: {e}")
+        return pd.DataFrame()
+    finally:
+        if connection is not None:
+            connection.close()
 
 def execute_queries_from_file(file_path):
     connection = None
@@ -49,4 +72,17 @@ def execute_queries_from_file(file_path):
             print("\nConnection closed.")
 
 if __name__ == "__main__":
-    execute_queries_from_file("queries.sql")
+    """execute_queries_from_file("queries.sql")"""
+
+    tables = ["artists", "events"]
+
+    dataframes_dict = {}
+    for table in tables:
+        df = fetch_table_as_df(table)
+        if not df.empty:
+            dataframes_dict[table.capitalize()] = df
+
+    if dataframes_dict:
+        export_to_excel(dataframes_dict, "report.xlsx")
+    else:
+        print("No data fetched from database.")
